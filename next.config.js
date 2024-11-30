23be7af7329aff5fee67e3caa1dea1fd7458b72c
gtamilvanan17 <tamilbecse139@gmail.com>
2024-11-30 14:25:38 +0530

    Updated Pipelines

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // Check if running on GitHub Pages
+const repoName = 'valaithzham-portfolio'; // Replace with your repository name
+
 const nextConfig = {
   output: 'export',
+  basePath: isGitHubPages ? `/${repoName}` : '', // Set basePath only for GitHub Pages
+  assetPrefix: isGitHubPages ? `/${repoName}/` : '', // Prefix for static assets
   images: { 
     unoptimized: true,
     remotePatterns: [
@@ -15,7 +20,7 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
 };
 
 module.exports = nextConfig;
