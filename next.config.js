3b0f7635addce6fc533ce622a74a879000d1a789
santheesh-14597 <santheesh.av@zohocorp.com>
2025-01-01 01:27:44 +0530

    cross origin fix added in next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,37 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // Check if running on GitHub Pages
-const repoName = 'valaithzham-portfolio'; // Replace with your repository name
+const isGitHubPages = process.env.GITHUB_PAGES === "true"; // Check if running on GitHub Pages
+const repoName = "valaithzham-portfolio"; // Replace with your repository name
 
 const nextConfig = {
-  output: 'export',
-  basePath: isGitHubPages ? `/${repoName}` : '', // Set basePath only for GitHub Pages
-  assetPrefix: isGitHubPages ? `/${repoName}/` : '', // Prefix for static assets
-  images: { 
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "POST, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
+  output: "export",
+  basePath: isGitHubPages ? `/${repoName}` : "", // Set basePath only for GitHub Pages
+  assetPrefix: isGitHubPages ? `/${repoName}/` : "", // Prefix for static assets
+  images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      }
-    ]
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
 };
 
