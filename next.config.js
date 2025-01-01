27312557a4152153cf77d0f149bdc50b3c170d2d
santheesh-14597 <santheesh.av@zohocorp.com>
2025-01-01 10:09:19 +0530

    next config header export removed

--- next.config.js
+++ next.config.js
@@ -1,38 +1,25 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_PAGES === "true"; // Check if running on GitHub Pages
-const repoName = "valaithzham-portfolio"; // Replace with your repository name
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // Check if running on GitHub Pages
+const repoName = 'valaithzham-portfolio'; // Replace with your repository name
 
 const nextConfig = {
-  distDir: "build", // Specify the directory for the exported files
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "POST, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-        ],
-      },
-    ];
-  },
-  output: "export",
-  basePath: isGitHubPages ? `/${repoName}` : "", // Set basePath only for GitHub Pages
-  assetPrefix: isGitHubPages ? `/${repoName}/` : "", // Prefix for static assets
-  images: {
+  output: 'export',
+  basePath: isGitHubPages ? `/${repoName}` : '', // Set basePath only for GitHub Pages
+  assetPrefix: isGitHubPages ? `/${repoName}/` : '', // Prefix for static assets
+  images: { 
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ]
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
 };
 
