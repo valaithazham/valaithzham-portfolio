1eb1e140e5883d5932749906ffac2d7a3f1b15d2
gtamilvanan17 <tamilbecse139@gmail.com>
2025-01-10 21:34:43 +0530

    route api

--- next.config.js
+++ next.config.js
@@ -27,4 +27,17 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
+};
